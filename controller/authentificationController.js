e4b1522dbca9ceb2d1c2fabc8a7bc7675af8df98
souheilbenslama <benslamasouheil@gmail.com>
2021-06-17 10:52:54 +0100

    using mailgun from github student pack

--- controller/authentificationController.js
+++ controller/authentificationController.js
@@ -5,6 +5,9 @@ require("dotenv").config();
 var exports = module.exports = {};
 var smtpTransport = require("nodemailer-smtp-transport");
 var jwt = require("jsonwebtoken");
+var Mailgun = require('mailgun-js');
+
+
 
 exports.login = function(req, res, next) {
     User.authenticate(req.body.email,req.body.password,function(error,user){
@@ -62,15 +65,59 @@ exports.register = function(req, res, next) {
 }
   
 exports.forgetPassword = function(req,res,next){
+    console.log("hello world") ;
     User.findOne({email:req.body.email},function(err,user){
         if(err){
             err.message = "user not found";
             return next(err);
         }else if(!user){
+            
                 var error = new Error("mail not found!");
                 return res.status(401).send(error.message);
         }else{
-            var transporter = nodemailer.createTransport(smtpTransport({
+            
+
+//We pass the api_key and domain to the wrapper, or it won't be able to identify + send emails
+
+var mailgun = new Mailgun({apiKey: "548bd0d958e911ef9ab0be9d6c8fa81e-24e2ac64-94e69bcd", domain: "sandbox4e4467e881824450bcb2ed4ff368fa49.mailgun.org"});
+var code = Math.floor(Math.random()*899999)+100000;
+var codeData = {
+    value:code,
+    owner:user.email
+};
+ResetCode.create(codeData,function(error){ 
+    if(error){
+        error.message="code not saved successfully!";
+        return next(error);
+    }
+});    
+
+var data = {
+    //Specify email data
+      from: "petsi760@gmail.com",
+    //The email to contact
+      to: req.body.email,
+    //Subject and text data  
+    subject: "Verification Code",
+    text: "Your code is "+code
+    }
+    
+    //Invokes the method to send emails given the above data with the helper library
+    mailgun.messages().send(data, function (err, body) {
+        //If there is an error, render the error page
+        if (err) {
+            console.log(err) ;
+            err.message="mail not sent";
+                    return next(err);
+        }
+        //Else we can greet    and leave
+        else {
+            //Here "submitted.jade" is the view file for this landing page 
+            //We pass the variable "email" from the url parameter in an object rendered by Jade
+            res.send("code sent");
+        }
+    });
+           /* var transporter = nodemailer.createTransport(smtpTransport({
                 service:"Gmail",
                 auth: {
                     user: process.env.EMAIL,
@@ -104,7 +151,7 @@ exports.forgetPassword = function(req,res,next){
                 }else {
                     res.send("code sent");
                 }
-            });
+            });*/
         }
     });
 }
